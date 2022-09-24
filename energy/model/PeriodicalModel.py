8f42a15325fa74ad0d0b29dfcbb2a6eae905ac8d
Googol2002 <xingyuncao2002@outlook.com>
2022-09-24 20:09:08 +0800

    重新初始化了MLP的bias，计划修改dataset引入绝对时间，并加入1d-CNN提取特征。

--- energy/model/PeriodicalModel.py
+++ energy/model/PeriodicalModel.py
@@ -16,6 +16,12 @@ EPOCH_STEP = 400
 GRADIENT_NORM = 10
 WEIGHT_DECAY = 0.01
 
+
+def init_weights(layer):
+    if isinstance(layer, nn.Linear):
+        torch.nn.init.uniform_(layer.bias, 0, 2)
+
+
 class Bi_LSTM_MPL(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size, batch_size, means):
         super().__init__()
@@ -39,7 +45,11 @@ class Bi_LSTM_MPL(nn.Module):
             nn.ReLU(),
             nn.Linear(128, PERIOD)
         ).to(device)
+        self.mlp.apply(init_weights) # ReLU在负半轴会失活
         self.mlp[-1].bias = torch.nn.Parameter(torch.Tensor(means).to(device), requires_grad=False)
+
+    net = nn.Sequential(nn.Linear(2, 2), nn.Linear(2, 2))
+    net.apply(init_weights)
 
     def forward(self, input_seq):
         batch_size, seq_len = input_seq.shape[0], input_seq.shape[1]
@@ -80,7 +90,7 @@ def val_loop(dataloader, model, loss_fn):
             bias += torch.sum(torch.abs(pred - y) / y).item()
 
     val_loss /= (size * PERIOD)
-    log_printf("Bi-LSTM_MLP", f"Val Error: \n Bias: {(100 * bias / (size * PERIOD)):>0.1f}%, Avg loss: {val_loss:>8f} \n")
+    log_printf("Bi-LSTM_MLP", f"Val Error: \n Bias: {(100 * bias / (size * PERIOD)):>0.3f}%, Avg loss: {val_loss:>8f} \n")
 
     return val_loss, bias / size
 
