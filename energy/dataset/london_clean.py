88b2ea0b92ad371bc593550e05e2be86cad5f74c
fireball0213 <1306189897@qq.com>
2022-10-08 20:38:32 +0800

    新数据集更新类封装与测试调用示例

--- energy/dataset/london_clean.py
+++ energy/dataset/london_clean.py
@@ -9,80 +9,120 @@ import numpy as np
 import glob, os
 import random
 import datetime
+from torch.utils.data import Dataset, DataLoader
 
-SIZE = 20  # 随机读取规模
+"""
+最后的输出格式：X, y
+X呈型于L * W, y呈型于L' * W
+L 是序列长，L'是答案长,W是24h内的数据点(48)
+"""
 
-select_list = [random.randint(2, 5500) for i in range(SIZE)]  # 随机读取SIZE个文件
-select_file_list = [f"{i:0>5d}" for i in select_list]
-# print('select_list:',select_list)
+SIZE = 10
+Train_length=10
+Test_length=3
 
+class London_11_14(Dataset):
+    """
+    train_l：训练集天数
+    test_l：测试集天数
+    总天数约800+
+    size：随机可重复抽取样本的个数，可修改范围：10~5498
+    """
+    def __init__(self, train_l=Train_length, test_l=Test_length, size=SIZE):
+        self.train_l = train_l
+        self.test_l = test_l
+        self.size=size
 
-success_read_file = []
-for f in select_file_list:
-    file_name = "../london_clean/cleaned_household_MAC0" + str(f) + ".csv"
-    print(file_name)
-    if os.path.exists(file_name):
-        success_read_file.append(pd.read_csv(file_name, header=0, usecols=[4, 3], decimal=","))  # 读取每个表格
+        select_list = [random.randint(2, 5500) for i in range(self.size)]  # 随机读取SIZE个文件
+        select_file_list = [f"{i:0>5d}" for i in select_list]
 
-# 初始化求和表
-df_merge = pd.read_csv(r"../london_clean/cleaned_household_MAC000002.csv",
-                       header=0, usecols=[4, 3], decimal=",")
+        LOG_DIRECTORY="../../london_clean"
+        success_read_file = []
+        for f in select_file_list:
+            file_name = "cleaned_household_MAC0"+str(f) + ".csv"
+            file_name=os.path.join(LOG_DIRECTORY, file_name).replace('\\','/')
+            print(file_name)
+            if os.path.exists(file_name):
+                success_read_file.append(pd.read_csv(file_name, header=0, usecols=[4, 3], decimal=","))  # 读取每个表格
 
-# 合并
-values = ['_' + str(i) for i in range(6000)]  # 用于merge中重复列的重命名
-for i in range(len(success_read_file)):
-    df_merge = pd.merge(df_merge, success_read_file[i], how='outer', on='DateTime', sort=True,
-                        suffixes=('', values[i])).replace(
-        np.nan, 0)
-df_merge[df_merge.columns[1]] = 0
-# print(df_merge)
+        # 初始化求和表
+        df_merge = pd.read_csv("../../london_clean/cleaned_household_MAC000002.csv",
+                               header=0, usecols=[4, 3], decimal=",")
 
-# 求和
-while (df_merge.shape[1] >= 3):
-    df_merge[df_merge.columns[1]] = df_merge[df_merge.columns[1]].map(float) + \
-                                    df_merge[df_merge.columns[-1]].map(float)
-    df_merge[df_merge.columns[1]]=df_merge[df_merge.columns[1]].apply(lambda x: '%.4f' % x)
-    df_merge = df_merge.drop(df_merge.columns[-1], axis=1)
+        # 合并
+        values = ['_' + str(i) for i in range(6000)]  # 用于merge中重复列的重命名
+        for i in range(len(success_read_file)):
+            df_merge = pd.merge(df_merge, success_read_file[i], how='outer', on='DateTime', sort=True,
+                                suffixes=('', values[i])).replace(
+                np.nan, 0)
+        df_merge[df_merge.columns[1]] = 0
+        print(df_merge)
 
-
-# 添加周&月独热编码
-def get_one_hot(index, size):
-    '''
-    获得一个one-hot的编码
-    index:编码值
-    size:编码长度
-    '''
-    one_hot = [0 for _ in range(1, size + 1)]
-    one_hot[index - 1] = 1
-    return one_hot
-
-
-def add_one_hot_week(df):
-    week = list(df['DateTime'])
-    Week = []
-    for i in week:
-        a = int(i[0:4])
-        b = int(i[5:7])
-        c = int(i[8:10])
-        week_num = datetime.date(a, b, c).isoweekday()
-        print()
-        Week.append(get_one_hot(week_num, 7))
-    df['Week'] = Week
+        # 求和
+        while (df_merge.shape[1] >= 3):
+            df_merge[df_merge.columns[1]] = df_merge[df_merge.columns[1]].map(float) + \
+                                            df_merge[df_merge.columns[-1]].map(float)
+            df_merge[df_merge.columns[1]]=df_merge[df_merge.columns[1]].apply(lambda x: '%.4f' % x)
+            df_merge = df_merge.drop(df_merge.columns[-1], axis=1)
 
 
-def add_one_hot_month(df):
-    month = list(df['DateTime'])
-    Month = []
-    for i in month:
-        j = int(i[5:7])
-        Month.append(get_one_hot(j, 12))
-    df['Month'] = Month
+        # 添加周&月独热编码
+        def get_one_hot(index, size):
+            '''
+            获得一个one-hot的编码
+            index:编码值
+            size:编码长度
+            '''
+            one_hot = [0 for _ in range(1, size + 1)]
+            one_hot[index - 1] = 1
+            return one_hot
 
 
-add_one_hot_week(df_merge)  # 添加周独热编码
-add_one_hot_month(df_merge)  # 添加月独热编码
+        def add_one_hot_week(df):
+            week = list(df['DateTime'])
+            Week = []
+            for i in week:
+                a = int(i[0:4])
+                b = int(i[5:7])
+                c = int(i[8:10])
+                week_num = datetime.date(a, b, c).isoweekday()
+                Week.append(get_one_hot(week_num, 7))
+            df['Week'] = Week
 
-# 输出
-print(df_merge)
-outputpath='../dataset/example.csv'
-df_merge.to_csv(outputpath,sep=',',index=False)
+
+        def add_one_hot_month(df):
+            month = list(df['DateTime'])
+            Month = []
+            for i in month:
+                j = int(i[5:7])
+                Month.append(get_one_hot(j, 12))
+            df['Month'] = Month
+
+
+        add_one_hot_week(df_merge)  # 添加周独热编码
+        add_one_hot_month(df_merge)  # 添加月独热编码
+
+
+        self.data_all=df_merge
+        self.data_only=df_merge[df_merge.columns[1]]#只保留用电量数据
+        self.days=int(self.data_only.shape[0]/48)
+        self.counts=self.days-test_l-train_l+1#(X,y)总行数
+        self.dataset=np.array(self.data_only.values.tolist())
+
+
+        # 输出
+        #print(df_merge)
+        # outputpath='../../dataset/example.csv'
+        # df_merge.to_csv(outputpath,sep=',',index=False)
+    def __len__(self):
+        return self.counts
+    def __getitem__(self, index):
+        assert(index<self.__len__())
+        row_offset = index * 48
+        x, y = self.dataset[row_offset: row_offset + self.train_l *48], \
+               self.dataset[row_offset +self.train_l *48: row_offset + (self.train_l +self.test_l) *48]
+        x = x.reshape(self.train_l, 48)
+        y=y.reshape(self.test_l,48)
+        return x, y
+
+
---
+++ energy/tests/TestLondon.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*- #
+"""
+@filename:TestLondon.py
+@author:201300086
+@time:2022-10-06
+"""
+import random
+import pytest
+import numpy as np
+import matplotlib.pyplot as plt
+
+from energy.dataset.london_clean import London_11_14
+
+dataset = London_11_14(train_l=10, test_l=1, size=20)
+"""
+    train_l：训练集天数
+    test_l：测试集天数
+    总天数约800+
+    size：随机可重复抽取样本的个数，可修改范围：10~5498
+"""
+print(dataset.data_all)
+print(dataset.dataset)
+print(dataset.days)
+print(dataset.__len__())
+print(dataset.__getitem__(dataset.__len__() - 1))
+"""
+    __getitem__()传参范围range(dataset.__len__())
+    越界会assert
+"""
