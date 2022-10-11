61919846bd51a60fa26126935d61daf989c90259
fireball0213 <1306189897@qq.com>
2022-10-11 11:48:03 +0800

    更新非零分布统计

--- energy/dataset/london_merge.py
+++ energy/dataset/london_merge.py
@@ -5,11 +5,14 @@
 @time:2022-10-10
 """
 import pandas as pd
-df=pd.read_csv('dataset/london_merge_all.csv', header=0, decimal=",",
-               na_filter=False,)
-df_date=df[df.columns[0]]
-df_date=df_date.drop(df_date.index[:1920],axis=0)
-print(df_date)
+import numpy as np
+# df=pd.read_csv('dataset/london_merge_all.csv', header=0, decimal=",",
+#                na_filter=False,)
+# df_date=df[df.columns[0]]
+# df_date=df_date.drop(df_date.index[:1920],axis=0)
+# print(df_date)
+
+
 # 输出
 # outputpath='dataset/london_date.csv'
 # df_date.to_csv(outputpath,sep=',',index=False)
@@ -19,3 +22,18 @@ print(df_date)
 # # 输出
 # outputpath='dataset/london_data.csv'
 # df_data.to_csv(outputpath,sep=',',index=False)
+
+
+#数据集非0部分分布
+data=np.load('dataset/london_data.npy')
+data=data.astype(bool).astype(int)
+data_distribute=np.sum(data,axis=1)
+print(data_distribute.shape)
+print(data_distribute)
+import matplotlib
+import matplotlib.pyplot as plt
+dataset_axis = np.arange(data_distribute.shape[0])
+#dataset绘图
+plt.plot(dataset_axis, data_distribute, c='red', label='counts')
+plt.legend()
+plt.show()
