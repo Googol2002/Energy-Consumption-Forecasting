b2a8eac4e745a360199d34e19d367c4050afc7f6
fireball0213 <1306189897@qq.com>
2022-10-17 02:56:47 +0800

    直接预测函数封装，直接周预测bug修复

--- energy/tests/TestLondon.py
+++ energy/tests/TestLondon.py
@@ -15,46 +15,44 @@ import pandas as pd
 from energy.dataset.london_clean import London_11_14, London_11_14_random_select  # ,London_11_14_set_train
 from energy.dataset.london_clean import London_11_14_set_test, London_11_14_set, createDataSet
 
-# dataset = London_11_14(train_l=5, test_l=1, size=1000)
+def test_London_11_14():
+    dataset = London_11_14(train_l=5, test_l=1, size=1000)
+    df=London_11_14(train_l=5, test_l=1, size=200).before_sum
+    print(df)
 
-# test:
-# df=London_11_14(train_l=5, test_l=1, size=200).before_sum
-# print(df)
+def test_London_11_14_random_select():
+    dataset = London_11_14_random_select(train_l=7, test_l=7, size=3000)
+    print(len(dataset))
+    print(dataset[len(dataset)-1])
+    expectations,variances=dataset.statistics(110,7)
+    print("expectations:",expectations)
+    print("variances:",variances)
+    print(expectations.mean())
+    print(variances.mean())
+    print(dataset.dataset_all_mean)
+    print(dataset.dataset_all_var)
+    print(dataset.dataset_all_std)
+    print(dataset.days)
+    print(type(dataset.dataset),dataset.dataset.shape)
+    print(type(dataset))
 
-# df_data=pd.read_csv('dataset/london_data.csv', header=0, decimal=",",na_filter=False,dtype='float32')
-# data = np.genfromtxt('dataset/london_data.csv', dtype=float, delimiter=',', names=True)
-# t_begin = time.time()
-# data=np.load('dataset/london_data.npy')
-# df_data=pd.DataFrame(data)
-# df_date=pd.read_csv('dataset/london_date.csv', header=0, decimal=",",na_filter=False)
-# #t_end1 = time.time()
-# #print("time for load:", t_end1 - t_begin)
-# df=pd.merge(df_date,df_data,how='outer',right_index=True,left_index=True)
-# df[df.columns[1:]] = df[df.columns[1:]].astype(float)
-# t_end2 = time.time()
-# print("time for concat:", t_end2 - t_end1)
+def test_London_11_14_set():
+    data_set=London_11_14_set(train_l=5, label_l=1, size=3000,times=10)#time for set: 11.827157974243164
+    print(data_set[len(data_set)-1])#getitem访问
+    print(len(data_set))
+    expectations,variances=data_set.statistics()
+    print("expectations:",expectations)
+    print("variances:",variances)
+    print(data_set.lst[1])#访问元组示例:(x, y, x_1, y_1)
+    print(data_set.arr.shape)#(4820, 4)
+    print(data_set.arr[1])#<class 'numpy.ndarray'>[x, y, x_1, y_1]
+    print(data_set.counts)#元组个数:4820=10*(488-5-1)
+    print(data_set.set.dataset.shape)#已对纯数据取并集，所以不再是48的倍数
+    expectations = np.mean(data_set.set.dataset.reshape(-1, 48), axis=0)
+    variances = np.var(data_set.set.dataset.reshape(-1, 48), axis=0)
 
-
-dataset = London_11_14_random_select(train_l=7, test_l=7, size=3000)
-# # print(len(dataset))
-# # print(dataset[len(dataset)-1])
-# expectations,variances=dataset.statistics(110,7)
-# print("expectations:",expectations)
-# print("variances:",variances)
-# print(expectations.mean())
-# print(variances.mean())
-# print(dataset.dataset_all_mean)
-# print(dataset.dataset_all_var)
-# print(dataset.dataset_all_std)
-# print(dataset.days)
-
-"""
-    __getitem__()传参范围range(dataset.__len__())
-    越界会assert
-"""
-# print(type(dataset.dataset),dataset.dataset.shape)
-# print(type(dataset))
-"""
+def test_createDataSet():
+    """
     :param train_l：X天数
     :param label_l：y天数
     :param test_days：测试集组数（不参与数据增强），实际占天数label_l*test_days
@@ -63,34 +61,16 @@ dataset = London_11_14_random_select(train_l=7, test_l=7, size=3000)
     :param size: 随机抽取的用户数量，上限5068
     :param ev_key: 期望和方差的统计长度，=1代表一天48列，=7代表一周48*7列
     测试集个数=label_l*test_days*test_continuous
-"""
-set1, set2, expectations, variances = createDataSet(train_l=10, label_l=7, test_days=3, test_continuous=5, size=3000,
-                                                    times=1, ev_key=1)
-print("expectations_mean:", expectations.mean())
-print("variances_mean:", variances.mean())
-# print(set2[0])
-print("train_lens:", len(set1), "test_lens:", len(set2))
-#print(set1[1])
-
-# data_set=London_11_14_set(train_l=5, label_l=1, size=3000,times=10)#time for set: 11.827157974243164
-# print(data_set[len(data_set)-1])#getitem访问
-# print(len(data_set))
-# expectations,variances=data_set.statistics()
-# print("expectations:",expectations)
-# print("variances:",variances)
+    """
+    set1, set2, expectations, variances = createDataSet(train_l=10, label_l=7, test_days=3, test_continuous=5, size=3000,
+                                                        times=1, ev_key=1)
+    print("expectations_mean:", expectations.mean())
+    print("variances_mean:", variances.mean())
+    print(set2[0])
+    print("train_lens:", len(set1), "test_lens:", len(set2))
+    print(set1[1])
 
 
-# print(data_set.lst[1])#访问元组示例:(x, y, x_1, y_1)
-# print(data_set.arr.shape)#(4820, 4)
-# print(data_set.arr[1])#<class 'numpy.ndarray'>[x, y, x_1, y_1]
-# print(data_set.counts)#元组个数:4820=10*(488-5-1)
-
-
-# print(data_set.set.dataset.shape)#已对纯数据取并集，所以不再是48的倍数
-# expectations = np.mean(data_set.set.dataset.reshape(-1, 48), axis=0)
-# variances = np.var(data_set.set.dataset.reshape(-1, 48), axis=0)
-# print(expectations)
-# print(variances)
 
 ##直接用日或周均值来预测的效果
 def direct_predict(ev_key=1,epoch=10):
@@ -103,14 +83,13 @@ def direct_predict(ev_key=1,epoch=10):
         accuracy_t = 0
         times=0
         for i in range(len(dataset)):
-            if(ev_key>0 and i%ev_key==0):#dataset是一天断的，跳开才有意义
-                x, y, x1, y1 = dataset[i]
-                e,_=dataset.statistics(ev_key)#=1代表一天，=7代表一周
+            x, y, x1, y1 = dataset[i]
+            if(ev_key>0 and i%ev_key==0):#dataset相邻只有1天不同，跳开才有意义
+                e,_=dataset.statistics(ev_key)
                 e=e.reshape(-1,48)
-                accuracy_t += np.mean(1 - np.abs((e - y) / y))
+                accuracy_t += np.mean(1 - np.abs((e - y) / y))#ev_key=1时，自动把e（48，）横向复制到（48，7）
                 times+=1
             elif(ev_key==0):
-                x, y, x1, y1 = dataset[i]
                 accuracy_t += np.mean(1 - np.abs((x - y) / y))
                 times += 1
         print("[{}/{}]".format(j+1, epoch))
@@ -119,11 +98,15 @@ def direct_predict(ev_key=1,epoch=10):
         accuracy.append(accuracy_t)
 
     print(f"Accuracy: {accuracy} \nMean:{np.mean(accuracy)} using {ev_key} days of expectations")
-direct_predict(ev_key=7,epoch=20)
+
+test_createDataSet()
+direct_predict(ev_key=1,epoch=10)
+
+
+# #dataset绘图
 # START=0
 # END=dataset.days-1
 # dataset_axis = np.arange(dataset.dataset.shape[0])
-# #dataset绘图
 # plt.plot(dataset_axis[48*START:48*END], dataset.dataset[48*START:48*END], c='blue', label='X')
 # # #均值绘图
 # # plt.plot(dataset_axis[48*START:48*END:48], dataset.dataset_mean[START:END], c='green', label='X_mean')
